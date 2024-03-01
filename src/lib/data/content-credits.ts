0706be6bd755b27312dbd1c71ebf4df2a17ab848
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-01 22:54:21 +0600

    Add content credit data

---
+++ src/lib/data/content-credits.ts
@@ -0,0 +1,14 @@
+import type { ContentCreditType } from '$lib/types/content-credits';
+
+export const credits: ContentCreditType[] = [
+	{
+		name: 'Quran.com',
+		url: 'https://quran.com',
+		for: ['Surahs in Arabic', 'Translation by A. Yusuf Ali', 'Surah transliteration']
+	},
+	{
+		name: 'MyIslam.org',
+		url: 'https://myislam.org/99-names-of-allah/',
+		for: ['99 names of Allah', 'Meanings/Attribute of each name']
+	}
+];
