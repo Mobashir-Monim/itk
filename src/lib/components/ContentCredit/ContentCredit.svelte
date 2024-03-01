a702a4d46b62da0b12452a1b7aa5a8473da1db4d
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-01 22:54:59 +0600

    Add content credit component

---
+++ src/lib/components/ContentCredit/ContentCredit.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { ContentCreditType } from '$lib/types/content-credits';
+
+	export let credit: ContentCreditType;
+</script>
+
+<div class="w-full flex flex-col gap-2 p-5 bg-slate-50/10 rounded-xl">
+	<div class="">
+		<h3 class="text-[2rem] w-full text-center">{credit.name}</h3>
+		{#if credit.url}
+			<a href={credit.url} target="_blank" class="w-full text-center line-clamp-1 text-[0.8rem]">
+				{credit.url}
+			</a>
+		{/if}
+	</div>
+	<div
+		class="w-full max-w-[450px] mx-auto text-[0.8rem] flex flex-col gap-2.5 p-2.5 bg-slate-50/10 rounded-xl"
+	>
+		<p class="text-center">Source for:</p>
+		<ul style="!list-disc">
+			{#each credit.for as item}
+				<li class="text-center w-full">{item}</li>
+			{/each}
+		</ul>
+	</div>
+</div>
