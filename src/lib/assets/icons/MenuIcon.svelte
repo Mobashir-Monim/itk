ab3d02f696fe52f59bfb6849ad0ed647a8341aaa
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-02 12:21:51 +0600

    Add icon assets for top nav

---
+++ src/lib/assets/icons/MenuIcon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let size: number = 48;
+	export let classes: string = 'fill-gray-600 dark:fill-gray-200';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	class="transit {classes}"
+	height={size}
+	width={size}
+>
+	<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+</svg>
---
+++ src/lib/assets/icons/MenuOpenIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let size: number = 48;
+	export let classes: string = 'fill-gray-600 dark:fill-gray-200';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	class="transit {classes}"
+	height={size}
+	width={size}
+>
+	<path
+		d="M120-240v-80h520v80H120Zm664-40L584-480l200-200 56 56-144 144 144 144-56 56ZM120-440v-80h400v80H120Zm0-200v-80h520v80H120Z"
+	/>
+</svg>
