049ccd55e0a983d686ebcb457d857f241b56370f
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-02 23:08:20 +0600

    Work on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import '../app.css';
 	import { fly } from 'svelte/transition';
+	import TopNav from '$lib/components/Nav/TopNav.svelte';
+	import NunitoFont from '$lib/assets/fonts/Nunito-VariableFont_wght.ttf';
 
-    export let data;
+	export let data;
 </script>
 
-{#key data.url}
-	<main
-		out:fly={{ x: 200, duration: 300 }}
-		in:fly={{ x: -200, duration: 300, delay: 300 }}
-		class="w-full min-h-screen p-5 transit"
-	>
-		<slot />
-	</main>
-{/key}
+<svelte:head>
+	<link rel="preload" href={NunitoFont} as="font" type="font/ttf" crossorigin="anonymous" />
+</svelte:head>
+
+<main class="w-full transit relative">
+	<TopNav />
+    <div class="h-[60px]"></div>
+	{#key data.url}
+		<div
+			class="w-full h-[calc(100vh-140px)] p-5 overflow-y-auto nsb"
+			out:fly={{ x: 200, duration: 300 }}
+			in:fly={{ x: -200, duration: 300, delay: 300 }}
+		>
+			<slot />
+		</div>
+	{/key}
+</main>
