07696d561e1147504db031e1277431c8fd781559
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-02 12:20:51 +0600

    Update credits page

--- src/lib/data/content-credits.ts
+++ src/lib/data/content-credits.ts
@@ -3,12 +3,12 @@ import type { ContentCreditType } from '$lib/types/content-credits';
 export const credits: ContentCreditType[] = [
 	{
 		name: 'Quran.com',
-		url: 'https://quran.com',
+		url: 'quran.com',
 		for: ['Surahs in Arabic', 'Translation by A. Yusuf Ali', 'Surah transliteration']
 	},
 	{
 		name: 'MyIslam.org',
-		url: 'https://myislam.org/99-names-of-allah/',
+		url: 'myislam.org',
 		for: ['99 names of Allah', 'Meanings/Attribute of each name']
 	}
 ];
--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -3,7 +3,7 @@
 	import ContentCredit from '$lib/components/ContentCredit/ContentCredit.svelte';
 </script>
 
-<div class="w-full flex flex-col gap-10 justify-center min-h-[calc(100vh-2.5rem)]">
+<div class="w-full flex flex-col gap-10 justify-center my-auto">
 	<p class="text-center text-[2rem]">Thanks to</p>
 	{#each credits as credit}
 		<ContentCredit {credit} />
