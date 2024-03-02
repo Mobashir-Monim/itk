88dc38cae68948e74c76651c7247b6ddf0ba8159
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-02 23:07:11 +0600

    Update nav types and ds

--- src/lib/data-structures/Nav/NavItem.ts
+++ src/lib/data-structures/Nav/NavItem.ts
@@ -1,22 +1,62 @@
+import { goto } from '$app/navigation';
 import type { NavItemConstructorType, NavItemType } from '$lib/types/nav';
 
 class NavItem implements NavItemType {
 	name: string;
 	type: 'function' | 'route' | 'placeholder';
-	display?: (() => boolean) | undefined;
+	display: () => boolean;
 	subItems: NavItemType[];
 	expanded: boolean = false;
+	route?: string | undefined;
+	params?: Record<string, string> | undefined;
+	call?: (() => any) | undefined;
 
-	constructor({ name, type, display, subItems = [] }: NavItemConstructorType) {
+	constructor({
+		name,
+		type,
+		display = () => true,
+		subItems = [],
+		call,
+		route,
+		params
+	}: NavItemConstructorType) {
 		this.name = name;
 		this.type = type;
 		this.subItems = subItems;
+		this.display = display;
 
-		if (display) this.display = display;
+		if (type === 'route' && (!route || !params))
+			throw new Error('Route and params must be passed for route type');
+
+		if (type === 'function' && !call) throw new Error('Call must be passed for route type');
 	}
 
 	toggleExpansion() {
 		this.expanded = !this.expanded;
+	}
+
+	getRoute() {
+		if (this.route && this.params) {
+			let route = this.route;
+
+			for (const p in this.params) {
+				route.replace(p, this.params[p]);
+			}
+
+			return route;
+		}
+
+		return '';
+	}
+
+	executeClick() {
+		if (this.type === 'route') {
+			goto(this.getRoute());
+		} else if (this.call && this.type === 'function') {
+			return this.call();
+		} else {
+			this.toggleExpansion();
+		}
 	}
 }
 
--- src/lib/types/nav.ts
+++ src/lib/types/nav.ts
@@ -1,33 +1,23 @@
 export interface NavItemType {
 	name: string;
 	type: 'route' | 'function' | 'placeholder';
-	display?: () => boolean;
+	display: () => boolean;
 	expanded: boolean;
 	toggleExpansion: () => void;
-	subItems: (NavItemType | NavRouteItemType | NavFunctionItemType)[];
-}
-
-export interface NavRouteItemType extends NavItemType {
-	route: string;
-	params: string[];
-}
-
-export interface NavFunctionItemType extends NavItemType {
-	call: () => any | void;
+	subItems: NavItemType[];
+	route?: string;
+	params?: Record<string, string>;
+	call?: () => any | void;
+	getRoute: () => string;
+	executeClick: () => any | void;
 }
 
 export interface NavItemConstructorType {
 	name: string;
 	type: 'route' | 'function' | 'placeholder';
 	display?: () => boolean;
-	subItems: (NavItemType | NavRouteItemType | NavFunctionItemType)[];
-}
-
-export interface NavRouteConstructorItemType extends NavItemConstructorType {
-	route: string;
-	params: string[];
-}
-
-export interface NavFunctionConstructorItemType extends NavItemConstructorType {
-	call: () => any | void;
+	subItems: NavItemType[];
+	route?: string;
+	params?: string[];
+	call?: () => any | void;
 }
