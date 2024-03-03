a194a9cdcb088c6bb37455341b0015ba82fc6074
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-03 11:33:46 +0600

    Add capcitor packages

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mobashirmonim.itk',
+  appName: 'itk',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
