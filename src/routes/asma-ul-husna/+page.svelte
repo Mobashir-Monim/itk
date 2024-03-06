179e9841cd94ed89a5fc7aa352c12028f08e5644
Mobashir Monim <mobashirmonim@gmail.com>
2024-03-06 17:30:55 +0600

    Work on asma-ul-husna page

---
+++ src/routes/asma-ul-husna/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import SearchableList from '$lib/components/SearchableList/SearchableList.svelte';
+	import { namesOfAllahAzzaWaJal, type AllahsNameType } from '$lib/data/constants/asma-ul-husna';
+
+	const searchFunction = (search: string, name: AllahsNameType): boolean =>
+		name.meanings.map((m) => m.toLowerCase()).filter((m) => m.includes(search.toLowerCase()))
+			.length > 0 || name.name.toLowerCase().includes(search.toLowerCase());
+
+	const displayFunction = (name: AllahsNameType): string => `${name.name}`;
+
+	const onClickFunction = (name: AllahsNameType): void => {
+		console.log(`Clicked on ${name.name}`);
+
+		// goto(`/quran/${name.entry}`);
+	};
+</script>
+
+<SearchableList
+	items={namesOfAllahAzzaWaJal}
+	{searchFunction}
+	{displayFunction}
+	{onClickFunction}
+/>
